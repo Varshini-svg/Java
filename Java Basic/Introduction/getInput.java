f3bc37861f6578b0432936ac12fbe0f35fa76345
Varshini-svg <varshiniganesh999@gmail.com>
2025-08-29 19:49:01 +0530

    Delete Java Basic/Introduction directory

--- Java Basic/Introduction/getInput.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-public class getInput {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println(input.nextInt());
-    }
-}
